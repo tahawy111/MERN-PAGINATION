7e7425dc72c341f2efd67cc6bd12fe5602b627c3
tahawy111 <amer.vib582@gmail.com>
2022-11-27 00:54:07 +0200

    Finish

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./App.css";
-import Card from "./components/Card";
-import { Pagination } from "@mui/material";
+// import Card from "./components/Card";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Pagination,
+  Typography,
+} from "@mui/material";
 
 function App() {
   const pageNumber = useParams().pageNumber || 1;
@@ -43,7 +50,46 @@ function App() {
       />
       <div className="app__posts">
         {posts.map((post) => (
-          <Card key={post._id} post={post} />
+          <Card
+            style={{
+              height: "320px",
+              margin: "10px",
+              borderRadius: "20px",
+              overflow: "auto",
+            }}
+            key={post._id}
+            variant="outlined"
+          >
+            <React.Fragment>
+              <CardContent>
+                <Typography
+                  style={{ maxWidth: "fit-content" }}
+                  variant="h5"
+                  component="div"
+                >
+                  {post.title}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  {post.author}
+                </Typography>
+                <Typography variant="body2" style={{ width: "200px" }}>
+                  {post.body}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">
+                  <a
+                    href="https://youtube.com/@tahawy111"
+                    style={{ textDecoration: "none", color: "inherit" }}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    My Youtube Channel
+                  </a>
+                </Button>
+              </CardActions>
+            </React.Fragment>
+          </Card>
         ))}
       </div>
       <Pagination
@@ -54,6 +100,19 @@ function App() {
         style={{ margin: "20px 0" }}
         onChange={(e, value) => setPage(value)}
       />
+
+      <footer className="footer">
+        <h3>Welcome to my Channel "عامر الطحاوي"</h3>
+        <a
+          href="https://youtube.com/@tahawy111"
+          target="_blank"
+          style={{ color: "#198bde", fontSize: "20px" }}
+          rel="noopener noreferrer"
+        >
+          https://youtube.com/@tahawy111
+        </a>
+        <p>Copyright &copy; {new Date().getFullYear()}</p>
+      </footer>
     </div>
   );
 }
